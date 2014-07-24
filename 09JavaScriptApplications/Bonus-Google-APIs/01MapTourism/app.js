a234ad7b40e328c538013051f15472b560df2f7f
lostm1nd <kliomba@abv.bg>
2014-07-24 17:30:49 +0300

    Adding button and select for navigating through the cities

--- 09JavaScriptApplications/Bonus-Google-APIs/01MapTourism/app.js
+++ 09JavaScriptApplications/Bonus-Google-APIs/01MapTourism/app.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   var prevCityBtn = document.querySelector('#nav>.prev'),
       nextCityBtn = document.querySelector('#nav>.next'),
+      citySelect = document.querySelector('#nav>.cities'),
       mapContainer = document.getElementById('map'),
       cities = CityData.cities,
       cityInfo = CityData.cityInfo,
+      cityIndex = 0,
       geocoder = new google.maps.Geocoder(),
       mapZoom = 10,
       googleMap,
@@ -27,8 +29,36 @@ window.onload = function() {
     return deferred.promise;
   }
 
-  // Initialize map to the first city in the array
-  getCityCoordinatesPromise(cities[0])
+  function panMapTo(cityName) {
+    getCityCoordinatesPromise(cityName)
+      .then(function success(results) {
+        googleMap.panTo(results.coords);
+        marker.setPosition(results.coords);
+        infoWindow = new google.maps.InfoWindow({
+          content: cityInfo[cities.indexOf(results.city)]
+        });
+      }, function error(msg) {
+        console.log(msg);
+      });
+  }
+
+  (function populateSelectInput() {
+    var fragment = document.createDocumentFragment(),
+        option = document.createElement('option');
+
+    cities.forEach(function(city) {
+      var clone = option.cloneNode(true);
+      clone.value = city;
+      clone.innerText = city;
+      fragment.appendChild(clone);
+    });
+
+    citySelect.appendChild(fragment);
+  }());
+
+  // Initialize map and the marker to the first city in the array
+  // also create a new info window with the city information
+  getCityCoordinatesPromise(cities[cityIndex])
     .then(function success(results) {
       googleMap = new google.maps.Map(mapContainer, {
         zoom: mapZoom,
@@ -53,6 +83,31 @@ window.onload = function() {
       console.log(msg);
     });
 
+  // Attach event handlers for navigation
+  prevCityBtn.addEventListener('click', function(){
+    cityIndex -= 1;
 
+    if (cityIndex < 0) {
+      cityIndex = cities.length - 1;
+    }
+
+    panMapTo(cities[cityIndex]);
+  }, false);
+
+  nextCityBtn.addEventListener('click', function(){
+    cityIndex += 1;
+
+    if (cityIndex === cities.length) {
+      cityIndex = 0;
+    }
+
+    panMapTo(cities[cityIndex]);
+  }, false);
+
+  citySelect.addEventListener('input', function() {
+    cityIndex = cities.indexOf(this.value);
+
+    panMapTo(cities[cityIndex]);
+  }, false);
 
 };
